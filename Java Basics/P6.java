aa35a64b4c02a32b0a78af5dcaab8dde54e8275c
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:43:16 +0530

    Create P6.java

---
+++ Java Basics/P6.java
@@ -0,0 +1,9 @@
+public class P6 {
+    static void printName() {
+        System.out.println("Sree Sivani Muddu");
+    }
+
+    public static void main(String[] args) {
+        printName();
+    }
+}
