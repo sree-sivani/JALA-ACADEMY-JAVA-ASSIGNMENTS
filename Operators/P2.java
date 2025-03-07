dc9d515110e10c2788f6f4d52db367cc79a5f2a9
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:44:45 +0530

    Create P2.java

---
+++ Operators/P2.java
@@ -0,0 +1,9 @@
+public class P2 {
+    public static void main(String[] args) {
+        int x = 40;
+        System.out.println("Pre-increment: " + (++x));
+        System.out.println("Post-increment: " + (x++));
+        System.out.println("Pre-decrement: " + (--x));
+        System.out.println("Post-decrement: " + (x--));
+    }
+}
