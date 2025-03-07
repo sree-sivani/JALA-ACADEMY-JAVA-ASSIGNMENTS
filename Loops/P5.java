533003ec22296abe5511efa22f9f5a064e3f0f7f
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:54:36 +0530

    Update P5.java

--- Loops/P5.java
+++ Loops/P5.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class P5 {
     public static void main(String[] args) {
-        int i = 10;
-        while (i <= 100) {
-            System.out.println(i);
-            i += 2;
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+
+        int largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        System.out.println("Largest number: " + largest);
+        sc.close();
     }
 }
