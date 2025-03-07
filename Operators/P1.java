4fc43d1f48135682eb8fc65ddb306c5983261fdb
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:44:17 +0530

    Create P1.java

---
+++ Operators/P1.java
@@ -0,0 +1,9 @@
+public class P1 {
+    public static void main(String[] args) {
+        int a = 70, b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
