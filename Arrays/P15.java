76bbc1a44ce293f0eda901b3e0d58d3eac68a800
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 08:08:17 +0530

    Create P15.java

---
+++ Arrays/P15.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class P15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int evenCount = 0, oddCount = 0;
+
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Even numbers count: " + evenCount);
+        System.out.println("Odd numbers count: " + oddCount);
+    }
+}
