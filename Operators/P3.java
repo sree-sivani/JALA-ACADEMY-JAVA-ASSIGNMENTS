d1e57f584d99d0ba2052047d11a4212b7b3668bc
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:45:15 +0530

    Create P3.java

---
+++ Operators/P3.java
@@ -0,0 +1,7 @@
+public class P3 {
+    public static void main(String[] args) {
+        int a = 40, b = 90;
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+    }
+}
