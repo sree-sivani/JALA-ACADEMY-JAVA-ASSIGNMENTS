628ed68a2d7a6380ab5fe689173e390015ba3274
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:41:32 +0530

    Create P4.java

---
+++ Java Basics/P4.java
@@ -0,0 +1,15 @@
+public class P4 {
+    public static void main(String[] args) {
+        int num = 10;
+        boolean flag = true;
+        char letter = 'A';
+        float pi = 3.14f;
+        double precise = 3.14159;
+
+        System.out.println("Integer: " + num);
+        System.out.println("Boolean: " + flag);
+        System.out.println("Char: " + letter);
+        System.out.println("Float: " + pi);
+        System.out.println("Double: " + precise);
+    }
+}
