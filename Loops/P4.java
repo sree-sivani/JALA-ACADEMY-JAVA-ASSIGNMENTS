04d8a02ebfbc81f2aa0a81d17ba759763e113f8f
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:54:03 +0530

    Update P4.java

--- Loops/P4.java
+++ Loops/P4.java
@@ -1,7 +1,10 @@
-public class P4 {
+public class OddEven {
     public static void main(String[] args) {
-        int a = 10, b = 80, c = 30;
-        int largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-        System.out.println("Largest number: " + largest);
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0)
+                System.out.println(i + " is Even");
+            else
+                System.out.println(i + " is Odd");
+        }
     }
 }
