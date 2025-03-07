1467812d42bc3b1eb96e6ae1968248ed878b0699
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:50:19 +0530

    Create P3.java

---
+++ Loops/P3.java
@@ -0,0 +1,6 @@
+public class P3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++)
+            System.out.println(i + " is " + (i % 2 == 0 ? "Even" : "Odd"));
+    }
+}
