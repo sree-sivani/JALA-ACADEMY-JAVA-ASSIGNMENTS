963927391a24882a550c4ee4daf45581d4fea06c
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 08:08:35 +0530

    Create P16.java

---
+++ Arrays/P16.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0], max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+
+        System.out.println("Difference between max and min: " + (max - min));
+    }
+}
