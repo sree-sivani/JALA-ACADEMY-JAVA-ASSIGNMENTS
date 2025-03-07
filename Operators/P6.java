109dbe920ff7909058f11825b08c05c0e905fb68
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:46:47 +0530

    Create P6.java

---
+++ Operators/P6.java
@@ -0,0 +1,9 @@
+public class P6 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a >= b: " + (a >= b));
+    }
+}
