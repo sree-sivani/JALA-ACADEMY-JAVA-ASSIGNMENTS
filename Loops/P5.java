31196586b45454f9d7a9756a553856d3415c8971
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:51:40 +0530

    Create P5.java

---
+++ Loops/P5.java
@@ -0,0 +1,9 @@
+public class P5 {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i <= 100) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}
