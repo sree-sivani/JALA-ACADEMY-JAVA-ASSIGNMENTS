f653daf7fa4480cab33de266accc32c8142070aa
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:56:34 +0530

    Create P9.java

---
+++ Loops/P9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        boolean isPrime = num > 1;
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(isPrime ? "Prime Number" : "Not a Prime Number");
+        sc.close();
+    }
+}
