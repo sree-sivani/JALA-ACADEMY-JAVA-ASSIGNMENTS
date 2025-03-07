f48976cc6d3f6b1229a5c4dee9e9cb6619c373e9
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:41:05 +0530

    Create P3.java

---
+++ Java Basics/P3.java
@@ -0,0 +1,16 @@
+public class P3 {
+    public static void main(String[] args) {
+        // Single-line comment
+
+        /* 
+           Multi-line comment
+           This comment spans multiple lines 
+        */
+
+        /** 
+         * Documentation comment 
+         * Used for generating documentation 
+         */
+        System.out.println("Comments");
+    }
+}
