75689eba4513722032436ffc818b1a4220d5b7e3
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:55:37 +0530

    Create P7.java

---
+++ Loops/P7.java
@@ -0,0 +1,9 @@
+public class P7 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i <= 10);
+    }
+}
