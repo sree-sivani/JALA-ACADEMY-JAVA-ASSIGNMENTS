18fe9df12d11331a9808f25e62ebef6a6a861aee
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:46:22 +0530

    Create P5.java

---
+++ Operators/P5.java
@@ -0,0 +1,8 @@
+public class P5 {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        System.out.println("AND: " + (a && b));
+        System.out.println("OR: " + (a || b));
+        System.out.println("NOT a: " + (!a));
+    }
+}
