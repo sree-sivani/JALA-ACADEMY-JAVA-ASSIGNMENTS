a126583b7d0d1f4673199e4e664b9e7736c7dc4d
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2025-03-07 07:48:33 +0530

    Create P7.java

---
+++ Operators/P7.java
@@ -0,0 +1,12 @@
+public class P7 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        if(a > b){
+          System.out.println("Larger : " + a);
+          System.out.println("Smaller : " + b);
+        }else{
+          System.out.println("Larger : " + b);
+          System.out.println("Smaller : " + a);
+        }
+    }
+}
